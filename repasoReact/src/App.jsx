c0c5fde9b64c6d11622819fef0af4964149aebfb
Rodrigo Santillan <santillanrodrigo234@gmail.com>
2023-11-12 17:36:35 -0300

    primer commit

---
+++ repasoReact/src/App.jsx
@@ -0,0 +1,19 @@
+export function App () {
+  return (
+    <article>
+      <header>
+        <img alt="el avatar de midudev" src="https://unavatar.io/midudev" />
+        <div>
+          <strong>Miguel Angel Duran</strong>
+          <span>@midudev</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
+    </article>
+  )
+}
---
+++ repasoReact/src/main.jsx
@@ -0,0 +1,12 @@
+//Un componente es una funcion que nos devuelve un elemento. Los componenten empiezan con mayuscula. Osea PascalCase
+//esto es para que React diferencie que es un elemento JSX y no elemento html
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <App />
+)
